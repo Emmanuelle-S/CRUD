306c3a61eae0d2c3381f9a92301ecc4ac8bf325f
Emmanuelle-S <emmanuelle-sanchez@hotmail.fr>
2024-06-13 15:27:20 +0200

    ajout des commentaires du CRUD

--- app.js
+++ app.js
@@ -28,38 +28,38 @@ app.get("/users", (req, res) => {
 
 app.post("/users", (req, res) => { // création d'une route pour les requêtes HTTP POST avec un URL /users
     const newUser = {
-        id: users.length + 1,
-        name: req.body.name,
-        email: req.body.email
+        id: users.length + 1, // Assigne un nouvel ID à l'utilisateur basé sur la longueur actuelle du tableau users
+        name: req.body.name, // Récupère le nom de l'utilisateur depuis le corps de la requête.
+        email: req.body.email // Récupère l'email de l'utilisateur depuis le corps de la requête
     }
-    users.push(newUser);
-    res.status(201).json(newUser)
+    users.push(newUser); // Ajoute le nouvel utilisateur au tableau users. Avec une BDD on aurait une auto incrémentation à chaque ajout d'utilisateur
+    res.status(201).json(newUser) // Définit le code de statut HTTP à 201 (Created) et envoie la réponse avec le nouvel utilisateur en format JSON
 })
 
 // Route pour modifie un utilisateur 
 
-app.put("/users/:id", (req, res) => {
-    const userId = parseInt(req.params.id);
-    const user = users.find(u => u.id === userId);
-    if (user) {
-        user.name = req.body.name || user.name;
-        user.email = req.body.email || user.email;
-        res.json(user);
+app.put("/users/:id", (req, res) => { // Création de la route PUT avec un url users/:id (id dynamique)
+    const userId = parseInt(req.params.id); // Convertit le paramètre id en nombre entier + récupère le paramètre id de l'URL
+    const user = users.find(u => u.id === userId); // Trouve l'utilisateur dans le tableau users dont l'ID correspond à userId
+    if (user) { // Vérifie si un utilisateur avec l'ID donné a été trouvé.
+        user.name = req.body.name || user.name; // Si req.body.name est défini, met à jour le nom de l'utilisateur. Sinon, conserve l'ancien nom
+        user.email = req.body.email || user.email; // pareil
+        res.json(user); // Envoie l'utilisateur mis à jour en format JSON
     } else {
-        res.status(404).send("Utilisateur non trouvé")
+        res.status(404).send("Utilisateur non trouvé") // Si l'utilisateur n'est pas trouvé, envoie une réponse avec le code de statut 404 (Not Found) et un message d'erreur
     }
 })
 
 // Route pour supprimer un utilisateur 
 
 app.delete("/users/:id", (req, res) => {
-    const userId = parseInt(req.params.id);
-    const userIndex = users.findIndex(u => u.id === userId);
-    if (userIndex !== -1) {
-        const deletedUser = users.splice(userIndex, 1);
-        res.json(deletedUser);
+    const userId = parseInt(req.params.id);// jusque là pareil que le PUT
+    const userIndex = users.findIndex(u => u.id === userId); // Trouve l'index de l'utilisateur dans le tableau users dont l'ID correspond à userId.
+    if (userIndex !== -1) { // Vérifie si un utilisateur avec l'ID donné a été trouvé
+        const deletedUser = users.splice(userIndex, 1); // Supprime un élément du tableau users à l'index userIndex et renvoie l'utilisateur supprimé.
+        res.json(deletedUser); // Envoie l'utilisateur supprimé en format JSON
     } else {
-        res.status(404).send('User not found');
+        res.status(404).send('User not found'); // Si l'utilisateur n'est pas trouvé, envoie une réponse avec le code de statut 404 (Not Found) et un message d'erreur.
     }
 })
 
